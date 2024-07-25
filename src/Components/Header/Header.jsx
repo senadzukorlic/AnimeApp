0333e3aec40cf079c8da6706ed4625c90b0c250d
AjtanaDjerlek <ajtanadjerlek123@gmail.com>
2024-07-26 01:18:36 +0200

    added Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,1 +1,15 @@
-export default function Header() {}
+
+import React from 'react';
+import logoH from './logoH.png'; // Uvoz slike
+import { headerStyle, logoStyle, h1Style } from './HeaderStyled'; // Uvoz stilova
+
+const Header = () => {
+    return (
+        <div style={headerStyle}>
+            <img src={logoH} alt="AnimeApp Logo" style={logoStyle} />
+            <h1 style={h1Style}>AnimeApp</h1>
+        </div>
+    );
+}
+
+export default Header;
