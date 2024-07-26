81c530e47e70414def241a029501ae19ca506906
AjtanaDjerlek <ajtanadjerlek123@gmail.com>
2024-07-26 02:04:29 +0200

    creating a footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,19 @@
-
 import React from 'react';
 import logoF from './logoF.png'; // Uvoz slike
-import { footerStyle, logoStyle, h1Style } from './FooterStyled'; // Uvoz stilova
+import { footerStyle, logoStyle, h1Style, navStyle, navLinkStyle, lineStyle } from './FooterStyled'; // Uvoz stilova
 
 const Header = () => {
     return (
         <div style={footerStyle}>
             <img src={logoF} alt="AnimeApp Logo" style={logoStyle} />
             <h1 style={h1Style}>AnimeApp</h1>
-
+            <nav style={navStyle}>
+                <a href="#home" style={navLinkStyle}>Home</a>
+                <a href="#anime" style={navLinkStyle}>Anime</a>
+                <a href="#manga" style={navLinkStyle}>Manga</a>
+                <a href="#about-us" style={navLinkStyle}>About Us</a>
+            </nav>
+            <div style={lineStyle}></div> {/* Dodaj liniju ispod */}
         </div>
     );
 }
