f4c7112ab8d1bf28558b8624527df4036e9148ab
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-07-28 01:38:28 +0200

    added funcionality for favoirite page

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,1 +1,14 @@
-export function Favorites() {}
+import { AnimeData } from "../../Context/Anime"
+import { useContext } from "react"
+
+export function Favorites() {
+  const { favoriteAnime } = useContext(AnimeData)
+
+  return (
+    <>
+      {favoriteAnime.map((item) => (
+        <img src={item.attributes.posterImage.original} />
+      ))}
+    </>
+  )
+}
