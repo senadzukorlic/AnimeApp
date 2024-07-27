ee727194e3d87566af93dd8d253aaf29fb180b4c
dinabecirovic <dinaabecirovic@gmail.com>
2024-07-27 16:16:47 +0200

    Created About Us Page

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,1 +1,67 @@
-export function AboutUs() {}
+import React from 'react';
+import ProfileCard from './profileCard'; // Adjust the import path as necessary
+import Image from '../AboutUs/download.png'; // Ensure this path points to your image file
+import { ProfileCardsContainer } from './AboutUsStyled'; // Ensure this path is correct
+
+export function AboutUs() {
+    const socialLinksSenad = [
+        { href: "#", iconClass: "fab fa-facebook-square" },
+        { href: "#", iconClass: "fab fa-instagram" },
+        { href: "https://github.com/senadzukorlic", iconClass: "fab fa-github" },
+        { href: "https://linkedin.com/in/senad-zukorlic-49343a276/", iconClass: "fab fa-linkedin" }
+    ];
+    const socialLinksDzenis = [
+        { href: "#", iconClass: "fab fa-facebook-square" },
+        { href: "#", iconClass: "fab fa-instagram" },
+        { href: "https://github.com/DzenisFejzovic", iconClass: "fab fa-github" },
+        { href: "https://linkedin.com/in/dzenis-fejzovic-ba0aa5316", iconClass: "fab fa-linkedin" }
+    ];
+    const socialLinksAjtana = [
+        { href: "#", iconClass: "fab fa-facebook-square" },
+        { href: "#", iconClass: "fab fa-instagram" },
+        { href: "https://github.com/AjtanaDjerlek", iconClass: "fab fa-github" },
+        { href: "https://linkedin.com/in/ajtana-djerlek-121264317", iconClass: "fab fa-linkedin" }
+    ];
+    const socialLinksHaris = [
+        { href: "#", iconClass: "fab fa-facebook-square" },
+        { href: "#", iconClass: "fab fa-instagram" },
+        { href: "https://github.com/harisbecirovic", iconClass: "fab fa-github" },
+        { href: "https://www.linkedin.com/in/haris-becirovic-142565267/", iconClass: "fab fa-linkedin" }
+    ];
+
+    return (
+        <ProfileCardsContainer>
+            <ProfileCard
+                image={Image}
+                name="Senad Zukorlic"
+                title="React Developer"
+                description="Senad is a mathematician, and he works at center NIT as a Web developer."
+                socialLinks={socialLinksSenad}
+            />
+            <ProfileCard
+                image={Image}
+                name="Dženis Fejzović"
+                title="React Developer"
+                description="Dženis is a mathematician, and he works at center NIT as a Web developer."
+                socialLinks={socialLinksDzenis}
+            />
+            <ProfileCard
+                image={Image}
+                name="Ajtana Djerlek"
+                title="React Developer"
+                description="Ajtana is a mathematician, and she works at center NIT as a Web developer."
+                socialLinks={socialLinksAjtana}
+            />
+            <ProfileCard
+                image={Image}
+                name="Haris Bećirović"
+                title="React Developer"
+                description="Haris is a mathematician, and he works at center NIT as a Web developer."
+                socialLinks={socialLinksHaris}
+            />
+        </ProfileCardsContainer>
+    );
+}
+
+export default AboutUs;
+
